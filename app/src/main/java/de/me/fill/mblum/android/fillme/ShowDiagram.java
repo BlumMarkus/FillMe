d7d567000f7e04160d3aa9c54a973d097c9f0fc7
Mathias <nomailforme@mail.com>
2017-09-02 17:04:18 +0200

    VerlaufDiagramm anzeige angelegt.

---
+++ app/src/main/java/de/me/fill/mblum/android/fillme/ShowDiagram.java
@@ -0,0 +1,13 @@
+package de.me.fill.mblum.android.fillme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowDiagram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_diagram);
+    }
+}
