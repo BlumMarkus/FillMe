1ef6f0529adc41e6ded5c2860aa7a987bfe21fc4
Mathias <noMailForMe@mail.com>
2017-09-05 23:29:16 +0200

    VerlaufDiagramm anzeige erweitert.

--- app/src/main/java/de/me/fill/mblum/android/fillme/ShowDiagram.java
+++ app/src/main/java/de/me/fill/mblum/android/fillme/ShowDiagram.java
@@ -1,13 +1,106 @@
 package de.me.fill.mblum.android.fillme;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class ShowDiagram extends AppCompatActivity {
+
+    private RelativeLayout diagramLayout;
+    private LineChart mChart;
+    private Description desc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_diagram);
+        diagramLayout = (RelativeLayout) findViewById(R.id.diagramLayout);
+        // create Line Chart
+        mChart = new LineChart(this);
+        //add to diagram Layout
+        diagramLayout.addView(mChart);
+
+        //customize line chart
+        desc = new Description();
+        desc.setText("");
+        mChart.setDescription(desc);
+        mChart.setNoDataText("Keine Daten vorhanden!");
+
+        //enable value highlighting
+        mChart.setHighlightPerTapEnabled(true);
+        mChart.setHighlightPerDragEnabled(true);
+
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        mChart.setPinchZoom(true);
+        mChart.setBackgroundColor(Color.LTGRAY);
+
+        //datas
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        mChart.setData(data);
+
+        Legend l = mChart.getLegend();
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+
+        XAxis x1 = mChart.getXAxis();
+        x1.setTextColor(Color.WHITE);
+        x1.setDrawGridLines(false);
+        x1.setAvoidFirstLastClipping(true);
+
+        YAxis y1 = mChart.getAxisLeft();
+        y1.setTextColor(Color.WHITE);
+        y1.setDrawGridLines(false);
+        y1.setDrawGridLines(true);
+
+        YAxis y12 = mChart.getAxisRight();
+        y12.setEnabled(false);
+    }
+
+    private void addEntry() {
+
+        LineData data = mChart.getData();
+
+        ILineDataSet set = data.getDataSetByIndex(0);
+        // set.addEntry(...); // can be called as well
+
+        if (set == null) {
+            set = createSet();
+            data.addDataSet(set);
+        }
+    }
+
+    private LineDataSet createSet() {
+        LineDataSet set = new LineDataSet(null, "DataSet 1");
+        set.setCubicIntensity(0.2f);
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(ColorTemplate.getHoloBlue());
+        set.setCircleColor(ColorTemplate.getHoloBlue());
+        set.setLineWidth(2f);
+        set.setCircleRadius(4f);
+        set.getFillAlpha();
+        set.setFillColor(ColorTemplate.getHoloBlue());
+        set.setHighLightColor(Color.rgb(244, 117, 177));
+        set.setValueTextColor(Color.WHITE);
+        set.setValueTextSize(10f);
+
+        return set;
     }
 }
