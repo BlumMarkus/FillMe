a024e600ccc5ff0b183d0998e6c656f73c5c023c
Markus Blum <privat@blum-markus.de>
2020-05-04 12:37:16 +0200

    + Added new design to settings activity + Added download icon + Added upload icon + Code styling

--- app/src/main/java/de/me/fill/mblum/android/fillme/SettingsActivity.java
+++ app/src/main/java/de/me/fill/mblum/android/fillme/SettingsActivity.java
@@ -6,15 +6,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -26,107 +26,149 @@ import java.util.ArrayList;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private TextView tv_Import_Data;
-    private TextView tv_Export_Data;
     private FillMeDataSource fmds;
-    private FillMeDbHelper fmdh;
     private StringBuilder data;
-    public static final int rquestcode = 1;
-    public Intent selectedFile;
 
+    public static final int rquestcode = 1;
+
+    public Intent selectedFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        tv_Import_Data = (TextView) findViewById(R.id.tv_Settings_Import_Button);
-        tv_Export_Data = (TextView) findViewById(R.id.tv_Settings_Export_Button);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == android.R.id.home) {
-            this.finish();
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        tv_Import_Data.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                import_Data();
-            }
-        });
-
-        tv_Export_Data.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                export_Data();
-            }
-        });
+        ImageButton btn_settings_cancel = findViewById(R.id.btn_settings_cancel);
+        Button btn_settings_exportData = findViewById(R.id.btn_settings_exportData);
+        Button btn_settings_importData = findViewById(R.id.btn_settings_importData);
 
         fmds = new FillMeDataSource(this);
-        fmdh = new FillMeDbHelper(this);
+
+        btn_settings_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btn_settings_exportData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exportData();
+            }
+        });
+
+        btn_settings_importData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                importData();
+            }
+        });
     }
 
-    private void import_Data()
-    {
+    @Override
+    protected void onActivityResult(int rquestcode, int resultCode, Intent intent) {
+        if (rquestcode == 1) {
+            selectedFile = intent;
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Warnung");
+            builder.setMessage("Wollen sie wirklich fortfahren diese Datei zu importieren?\nHierbei werden alle Daten ersetzt!");
+            builder.setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    readAndImportFile(selectedFile);
+                }
+            });
+
+            builder.setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Do nothing
+                    dialog.dismiss();
+                }
+            });
+
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 0:
+                generateCSVOnSD(this, "Export_Database.csv", data);
+            case 1:
+                Intent fileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                fileIntent.setType("*/*");
+                startActivityForResult(fileIntent, rquestcode);
+        }
+    }
+
+    /**
+     * Import database data from installed SD-Card
+     */
+    private void importData() {
         if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 Intent fileIntent = new Intent(Intent.ACTION_GET_CONTENT);
                 fileIntent.setType("text/*");
                 startActivityForResult(fileIntent, rquestcode);
-            }
-            else {
+            } else {
                 // Request permission from the user
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
             }
-        }
-        else {
+        } else {
             Toast.makeText(this, "Sie benötigen ein SD-Karte", Toast.LENGTH_SHORT).show();
         }
 
     }
 
-    private void export_Data()
-    {
+    /**
+     * Export database data as csv to Downloads/FillMe_Database folder
+     */
+    private void exportData() {
         data = new StringBuilder();
         ArrayList<FillEntry> list = new ArrayList<>();
         list = fmds.getAllEntries(false);
 
         //Header
-        data.append(String.format("%s,%s,%s,%s,%s", fmdh.COLUMN_DATE,fmdh.COLUMN_MILEAGE,fmdh.COLUMN_LITER,fmdh.COLUMN_PRICE, fmdh.COLUMN_STATUS));
+        data.append(String.format("%s,%s,%s,%s,%s", FillMeDbHelper.COLUMN_DATE, FillMeDbHelper.COLUMN_MILEAGE, FillMeDbHelper.COLUMN_LITER, FillMeDbHelper.COLUMN_PRICE, FillMeDbHelper.COLUMN_STATUS));
 
         //data
-        for (FillEntry entry : list){
-            String fillEntryString = String.format("\n%s,%s,%s,%s,%s", entry.getDate(), entry.getMileage(),entry.getLiter(),entry.getPrice(),entry.getStatus());
+        for (FillEntry entry : list) {
+            String fillEntryString = String.format("\n%s,%s,%s,%s,%s", entry.getDate(), entry.getMileage(), entry.getLiter(), entry.getPrice(), entry.getStatus());
             data.append(fillEntryString);
         }
 
         if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 generateCSVOnSD(this, "Export_Database.csv", data);
-            }
-            else {
+            } else {
                 // Request permission from the user
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
             }
-        }
-        else {
+        } else {
             Toast.makeText(this, "Sie benötigen ein SD-Karte", Toast.LENGTH_SHORT).show();
         }
     }
 
+    /**
+     * Creates the .csv file and fills it with the given data.
+     *
+     * @param context Current context
+     * @param sFileName Name of the csv file
+     * @param sBody Content of the file
+     */
     private void generateCSVOnSD(Context context, String sFileName, StringBuilder sBody) {
         try {
             File root = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "FillMe_Database");
@@ -138,14 +180,13 @@ public class SettingsActivity extends AppCompatActivity {
             writer.append(sBody);
             writer.flush();
             writer.close();
-            Toast.makeText(context, "Datenbank wurde in "+ '"'+ "Downloads/FillMe_Database" + '"' + " exportiert!", Toast.LENGTH_SHORT).show();
-        }
-        catch (IOException e) {
+            Toast.makeText(context, "Datenbank wurde in " + '"' + "Downloads/FillMe_Database" + '"' + " exportiert!", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void readAndImportFile (Intent intent){
+    private void readAndImportFile(Intent intent) {
         if (intent == null)
             return;
         fmds.deleteAllData();
@@ -172,51 +213,6 @@ public class SettingsActivity extends AppCompatActivity {
             Toast.makeText(this, "Datenbank wurde erfolreich Importiert!", Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int rquestcode, int resultCode, Intent intent)
-    {
-        if (intent == null)
-            return;
-        switch (rquestcode) {
-            case 1:
-                selectedFile = intent;
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Warnung");
-                builder.setMessage("Wollen sie wirklich fortfahren diese Datei zu importieren?\nHierbei werden alle Daten ersetzt!");
-                builder.setPositiveButton("Ja", new DialogInterface.OnClickListener() {
-
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        readAndImportFile(selectedFile);
-                    }
-                });
-
-                builder.setNegativeButton("Nein", new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // Do nothing
-                        dialog.dismiss();
-                    }
-                });
-
-                AlertDialog alert = builder.create();
-                alert.show();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case 0:
-                generateCSVOnSD(this, "Export_Database.csv", data);
-            case 1:
-                Intent fileIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                fileIntent.setType("*/*");
-                startActivityForResult(fileIntent, rquestcode);
         }
     }
 }
